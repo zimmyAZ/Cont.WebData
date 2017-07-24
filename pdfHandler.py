4e86ddaf9c365d419a0a851e1866616cfd7af29c
zimmyAZ <ATZimdars@gmail.com>
2017-07-24 16:49:54 -0700

    Update pdfHandler.py

--- pdfHandler.py
+++ pdfHandler.py
@@ -1,13 +1,10 @@
-import PyPDF2
-from pdf2jpg import pdf2jpg
+import tkFileDialog
+import os
 
 
-pdf_file = open('test.pdf')
-read_pdf = PyPDF2.PdFileReader(pdf_file)
-
-number_of_pages = read_pdf.getNumPages()
-print number_of_pages
-page = read_pdf.getPage(range(0, number_of_pages))
-pdf2jpg(pdf_file)
-
-
+f = tkFileDialog.askopenfilename()
+d = tkFileDialog.askdirectory()
+command = ("convert " + f + " " + d + "/output.png")
+DIR = ' + d + '
+os.system(command)
+images_converted = len(os.walk(d).next()[2])
